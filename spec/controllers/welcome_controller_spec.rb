e00268a78364ab3ce0e94cd705bbe8b1fce55856
occamskatana <johngallweycarter@gmail.com>
2015-11-09 19:15:46 -0500

    finally finally finally got rspec completely working

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe WelcomeController, type: :controller do
+ RSpec.describe WelcomeController, :type => :controller do
 
 		describe "GET index" do 
-			it "renders the index template"
+			it "renders the index template" do
 
 			get:index
 
 			expect(response).to render_template("index")
 		end
 	end
+
+
+	describe "GET about" do
+     it "renders the about template" do
+       get :about
+       expect(response).to render_template("about")
+     end
+   end  
+   
+end
 
 
