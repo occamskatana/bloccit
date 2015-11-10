740c348dbdb22534e9d3fe61e7be35c743c6b05a
occamskatana <johngallweycarter@gmail.com>
2015-11-10 12:32:03 -0500

    assignment 31 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
+
+  	@posts.each do |post|
+  		if post.id % 5 == 0
+  			post.title = "SPAM"
+  		else
+  			post
+
+  		end
+
+  	end
   end
 
   def show
