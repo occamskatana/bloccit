756a8e32b49904aef98ecae55cd8e4fe6f82dcf6
occamskatana <johngallweycarter@gmail.com>
2015-11-18 12:26:36 -0500

    checkpoint 39 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,11 +16,10 @@ class PostsController < ApplicationController
   end
 
   def create
-  	@post = Post.new
-  	@post.title = params[:post][:title]
-  	@post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+  	@post = @topic.posts.build(post_params)
+    
+    
     @post.user = current_user
 
   	if @post.save
@@ -40,8 +39,7 @@ class PostsController < ApplicationController
   def update
 
     @post = Post.find(params[:id])
-    @post.title = (params[:post][:title])
-    @post.body = (params[:post][:body])
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post was updated"
@@ -66,6 +64,10 @@ class PostsController < ApplicationController
     end
   end
 
+  private 
 
+  def post_params 
+    params.require(:post).permit(:title, :body)
+  end
 end
 
